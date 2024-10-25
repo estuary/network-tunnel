20b6d9a32a5572179fb88ace0b47a9ce92fa6d21
Mahdi Dibaiee <mdibaiee@pm.me>
2024-10-25 11:32:11 +0100

    clean up errors, print Display instead of Debug format of error

--- src/errors.rs
+++ src/errors.rs
@@ -1,27 +1,13 @@
-use base64::DecodeError;
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("io operation error.")]
-    IoError(#[from] std::io::Error),
-
-    #[error("Json serialization error.")]
-    JsonError(#[from] serde_json::Error),
-
-    #[error("Failed to decode base64 content of OpenSSH key.")]
-    DecodeError(#[from] DecodeError),
+    #[error("{0}")]
+    IO(#[from] std::io::Error),
 
     #[error("SSH forwarding network tunnel exit with non-zero exit code {0}")]
     TunnelExitNonZero(String),
 
-    #[error("network tunnel requested, but no destination address was found in the endpoint configuration")]
-    MissingDestinationAddress,
-
-    #[error("malformed destination address {0}")]
-    BadDestinationAddress(String),
-
-    // Used to silently terminate the SSH tunnel without logging any further errors
+    // Used to bubble up SSH tunnel errors without logging any further errors
     // this allows the last `ssh: ` log to be reported as the main error to the user
-    #[error("")]
-    SilentError
+    #[error("{0}")]
+    SSH(String)
 }
